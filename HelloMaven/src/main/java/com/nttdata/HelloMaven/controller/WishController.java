abb0ca3753196b6830af5dab0cd4bf6f7eb9a6bd
Marascioa <Marascioa@VT-MARASCIOA>
2020-07-29 15:54:26 +0200

    firtst commit

---
+++ HelloMaven/src/main/java/com/nttdata/HelloMaven/controller/WishController.java
@@ -0,0 +1,14 @@
+package com.nttdata.HelloMaven.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WishController {
+
+	@GetMapping("/welcome")
+	public String greeting() {
+		return "welcome at all";
+	}
+
+}

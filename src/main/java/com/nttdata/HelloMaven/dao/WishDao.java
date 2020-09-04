75a142737d4bc82fdfbd21fc516b2bfbb17803f0
Marascioa <Marascioa@VT-MARASCIOA>
2020-09-04 17:33:27 +0200

    ggggggggggg

--- src/main/java/com/nttdata/HelloMaven/dao/WishDao.java
+++ src/main/java/com/nttdata/HelloMaven/dao/WishDao.java
@@ -2,9 +2,7 @@ package com.nttdata.HelloMaven.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Vector;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,40 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
-import com.nttdata.HelloMaven.controller.WishController;
 import com.nttdata.HelloMaven.entity.Show;
 
-@Repository
+
 @Component
 public class WishDao {
 	
 	private static final Logger logger = LoggerFactory.getLogger(WishDao.class);
 	
-//	@Autowired
-//	JdbcTemplate jdbcTemplate;  
-//	
-//    
-//    public List findAll() throws Exception{
-//    	try {
-//	    	logger.info("Logging findAll - Start");
-//	    	List toRet = jdbcTemplate.query("select * from public.tabellatest", new ShowRowMapper());
-//	    	if(toRet!=null) logger.info("Logging findAll - End - size:"+toRet.size());
-//	    	else logger.warn("Logging findAll - END - Errore dati");
-//	    	return toRet;
-//    	}
-//    	catch(Exception ex) {
-//    		logger.error("Logging findAll - END",ex);
-//    		throw ex;
-//    	}
-//    	
-//    }
+	@Autowired
+	JdbcTemplate jdbcTemplate;  
+	
     
     public List findAll() throws Exception{
     	try {
 	    	logger.info("Logging findAll - Start");
-	    	List toRet = Arrays.asList(	"Pippo","Pluto","Paperino");
+	    	List toRet = jdbcTemplate.query("select * from public.tabellatest", new ShowRowMapper());
 	    	if(toRet!=null) logger.info("Logging findAll - End - size:"+toRet.size());
 	    	else logger.warn("Logging findAll - END - Errore dati");
 	    	return toRet;
@@ -56,7 +37,7 @@ public class WishDao {
     	}
     	
     }
-	
+    
 }
 
 
